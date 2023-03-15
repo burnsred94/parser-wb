19cbd58ac2467c949ba579163d6b408397fa04a4
Oleg Kamenshikov <burnsred94@gmail.com>
2023-03-15 12:25:29 +0300

    Fix connection mongo

--- bot-ai/src/database/mongo.config.ts
+++ bot-ai/src/database/mongo.config.ts
@@ -9,7 +9,7 @@ export const getMongoconfig =  (configService:ConfigService):MongooseModuleOptio
 }
 
 const getMongoConnectUri = (configService:ConfigService) => {
-    const uri= `mongo://${configService.get('MONGO_INITDB_ROOT_USERNAME')}:${configService.get('MONGO_INITDB_ROOT_PASSWORD')}@${configService.get('HOST_MONGO_DB')}:${Number(configService.get('MONGO_DB_PORT'))}`
+    const uri= `mongodb://${configService.get('MONGO_INITDB_ROOT_USERNAME')}:${configService.get('MONGO_INITDB_ROOT_PASSWORD')}@${configService.get('HOST_MONGO_DB')}:${Number(configService.get('MONGO_DB_PORT'))}`
     return uri
 }
 
